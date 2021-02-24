4bbcb5c6d4ca882053a19b2502424d1b7758df95
Ronaldo Ferreyra <rferreyra@reweb.com.br>
2021-02-24 17:48:49 -0300

    Aula 03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import styles from '../styles/pages/Home.module.css';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from "../components/ChallengeBox";
+
 import Head from 'next/head'
 
 export default function Home() {
@@ -11,7 +14,7 @@ export default function Home() {
       <Head>
         <title>Inicio | move.it</title>
       </Head>
-      
+
       <ExperienceBar />
 
       <section>
@@ -21,7 +24,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-          RIGHT
+          <ChallengeBox />
         </div>
       </section>
     </div>
